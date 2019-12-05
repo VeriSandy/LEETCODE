92bbf374da8d9a7f859aa8100ad39e4d326658e8
VeriSandy <7421926+VeriSandy@users.noreply.github.com>
2019-12-05 18:44:40 +0530

    Update 70_RightPointerInTree.c

--- 70_RightPointerInTree.c
+++ 70_RightPointerInTree.c
@@ -15,8 +15,27 @@ If there is no next right node, the next pointer should be set to NULL.
 Initially, all next pointers are set to NULL.
 */
 
+
+/*
+Ideas -- 
+1. Using level order traversal, use recursion.
+
+2. Using level order traversal, use queue method. 
+Take one queue, put root node and mark with one delimiter.
+while processing the queue, keep pushing the left and right child once you get the delimiter, push another delimter. 
+
+For next node - 
+While processing the queue 
+remove front node and mark front node in queue as next node of popped node
+If it is delimiter, mark next node as NULL.
+
+3. Use preorder approach(mostly correct for complete binary tree or perfect binary tree), reursive approach.
+
+4. Use Iterative approach.
+
+*/
+//1. Using level order traversal, use recursion.
 class Solution {
-    
 public:
     
     int Height(Node *root)
@@ -69,6 +88,84 @@ public:
             ConnectUtility(root, 0, i, &t);
             //printf("\n");   
         }
+        return root;
+    }
+};
+======================================================================================================
+ //3. Use preorder approach(mostly correct for complete binary tree or perfect binary tree), reursive approach.
+class Solution { 
+public:
+  Node* connect(Node* root)
+  {
+      if(root == NULL) return NULL;
+
+      if(root->left)
+          root->left->next = root->right;
+
+      if(root->right)
+          root->right->next = root->next?root->next->left:NULL;
+
+      connect(root->left);
+      connect(root->right);
+
+      return root;
+  }
+};
+======================================================================================================
+//4. Use Iterative approach
+class Solution { 
+public:
+    
+    
+    Node* getNextRight(Node* root)
+    {
+        Node *temp = root->next;
+        
+        while(temp)
+        {
+            if(temp->left) return temp->left;
+            if(temp->right) return temp->right;
+            temp = temp->next;
+        }
+        
+        return NULL;
+    }
+    
+    Node* connect(Node* root)
+    {
+        if(root == NULL) return NULL;
+        root->next = NULL;
+        
+        Node *p = root, *q = root;
+        
+        while(q)
+        {
+            p = q;
+            
+            while(p)
+            {
+                if(p->left)
+                {
+                    if(p->right)
+                        p->left->next = p->right;
+                    else
+                        p->left->next = getNextRight(p);
+                }
+
+                if(p->right)
+                    p->right->next = getNextRight(p);
+
+                p = p->next;
+            }
+         
+            if(q->left)
+                q = q->left;
+            else if(q->right)
+                q = q->right;
+            else
+                q = getNextRight(q);
+        }
+        
         return root;
     }
 };
