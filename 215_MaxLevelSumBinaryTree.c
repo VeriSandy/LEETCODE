1619dbe3de3da713933871430e478c879e144c4f
VeriSandy <7421926+VeriSandy@users.noreply.github.com>
2019-12-28 11:04:57 +0530

    Create 215_MaxLevelSumBinaryTree.c

---
+++ 215_MaxLevelSumBinaryTree.c
@@ -0,0 +1,60 @@
+/*
+1. Use level order sum, find sum at one level
+return min level of max sum.
+
+2. Use BFS + level order concept.
+BFS == visit the neighbors first. Then visit the neighbors of neighbors.
+i.e Indirectly level by level.
+*/
+
+int Height(struct TreeNode* root)
+{
+    if(root == NULL) return 0;
+    
+    int l = Height(root->left);
+    int r = Height(root->right);
+    
+    return l>r?l+1:r+1;
+}
+
+
+void BFS(struct TreeNode* root, int level, int *temp)
+{
+    if(root == NULL) return;
+    
+    temp[level] += root->val;
+    
+    BFS(root->left, level+1, temp);
+    BFS(root->right, level+1, temp);
+    
+    return ;
+}
+
+
+int maxLevelSum(struct TreeNode* root)
+{
+    if(root == NULL) return 0;
+    
+    int h = Height(root);//will give you no of levels.
+    
+    //get the sum at each level
+    int *temp = malloc(sizeof(int)*h);
+    
+    BFS(root, 0, temp);
+    
+    int i = 1, max = 0, level = 0;
+    
+    max = temp[0];
+    
+    while(i < h)
+    {
+        if(max < temp[i])
+        {
+            max = temp[i];
+            level = i+1;
+        }
+        i++;
+    }
+    
+    return level;
+}
