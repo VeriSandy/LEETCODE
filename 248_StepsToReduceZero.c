b6bb568e626d6ac968d012bdfa24f42eea06874b
VeriSandy <7421926+VeriSandy@users.noreply.github.com>
2020-02-14 02:26:53 +0530

    Create 248_StepsToReduceZero.c

---
+++ 248_StepsToReduceZero.c
@@ -0,0 +1,19 @@
+
+
+int numberOfSteps (int num)
+{
+    int count = 0;
+    while(num)
+    {
+        if(num%2)
+            num--;
+        else
+            num /= 2;
+        
+        count++;
+    }
+    
+    return count;
+}
+
+
